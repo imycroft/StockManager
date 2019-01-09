a6763b622bba68924148984b4a4c0d5e685bb510
walid <w.hafid@gmail.com>
2019-01-09 22:01:13 +0100

    add menu on toolbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,46 +1,60 @@
 <template>
   <nav>
     <v-toolbar flat app>
-        <v-toolbar-side-icon class="primary--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon class="primary--text" @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase primary--text">
         <span class="font-weight-light">cnla</span>
         <span>info</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-menu open-on-hover down offset-y>
+        <v-btn  flat slot="activator" color="primary" dark>
+            <!-- <v-icon left>menu</v-icon> -->
+            <span>MENU</span>
+            
+        </v-btn>
+
+        <v-list>
+          <v-list-tile v-for="(item, index) in links" :key="index" router :to="item.route">
+              <v-list-tile-action>
+            <v-icon class="dark--text">{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+
       <v-btn flat color="primary">
-          <span>Sign Out</span>
-          <v-icon right>exit_to_app</v-icon>
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
     <v-navigation-drawer v-model="drawer" app class="indigo">
-        <v-list>
-            <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
-                <v-list-tile-action>
-                    <v-icon class="white--text">{{ link.icon }}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content class="white--text"> 
-                    {{link.text}}
-                </v-list-tile-content>
-            </v-list-tile>
-        </v-list>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content class="white--text">{{link.text}}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            drawer: false,
-            links: [
-                {icon: 'dashboard', text: 'Dashboard', route:'/'},
-                {icon: 'store', text: 'Stock', route:'/stock'},
-                {icon: 'assignment', text: 'Forms', route:'/forms'},
-                {icon: 'shopping_cart', text: 'Supliers', route:'/supliers'},
-                {icon: 'group', text: 'Customers', route:'/customers'},
-
-            ]
-        }
-    }
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "store", text: "Stock", route: "/stock" },
+        { icon: "assignment", text: "Forms", route: "/forms" },
+        { icon: "shopping_cart", text: "Supliers", route: "/supliers" },
+        { icon: "group", text: "Customers", route: "/customers" }
+      ]
+    };
+  }
 };
 </script>
--- client/src/views/Forms.vue
+++ client/src/views/Forms.vue
@@ -1,56 +1,47 @@
 <template>
- <div class="new-command">
-   <h1 class="headline grey--text">Forms</h1>
-   <v-container class="my-5">
+  <div class="new-command">
+    <h1 class="headline grey--text">Forms</h1>
+    <v-container class="my-5">
       <v-layout row justify-center>
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <v-btn slot="activator" color="primary" dark>New delivery</v-btn>
-      <v-card>
-        <v-card-title>
-          <span class="headline">New delivery form</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Designation*" required></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Amount" required></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field
-                  label="Note"
-                  hint="Type some notes here"
-                  
-                ></v-text-field>
-              </v-flex>
-              
-              
-              
-            </v-layout>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" flat @click="dialog = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-layout>
-   </v-container>
-   </div>
- 
+        <v-dialog v-model="dialog" persistent max-width="600px">
+          <v-btn slot="activator" color="primary" dark>New delivery</v-btn>
+          <v-card>
+            <v-card-title>
+              <span class="headline">New delivery form</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container grid-list-md>
+                <v-layout wrap>
+                  <v-flex xs12 sm6>
+                    <v-select :items="items" label="Designation*" required></v-select>
+                  </v-flex>
+                  <v-flex xs12 sm6 md4>
+                    <v-text-field label="Amount" required></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 sm6 md4>
+                    <v-text-field label="Note" hint="Type some notes here"></v-text-field>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+              <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
+              <v-btn color="blue darken-1" flat @click="dialog = false">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
-  
-
-  export default {
-     data: () => ({
-      dialog: false
-    })
-  }
+export default {
+  data: () => ({
+    dialog: false,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"] //From backend
+  })
+};
 </script>
