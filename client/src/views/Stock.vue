433ac54aaafb50418b0b3058462ea69690dafb9f
walid <w.hafid@gmail.com>
2019-01-14 14:13:16 +0100

    Add tabs to stock

--- client/src/views/Stock.vue
+++ client/src/views/Stock.vue
@@ -3,15 +3,198 @@
    <h1 class="headline grey--text" >Stock</h1>
    <v-container class="my-5">
      
+      <v-text-field
+        append-icon="mic"
+        class="mx-3"
+        flat
+        label="Search"
+        prepend-inner-icon="search"
+        v-model="search"
+        solo-inverted
+      ></v-text-field>
+     <v-tabs
+    color="cyan"
+    dark
+    icons-and-text
+  >
+    <v-tabs-slider color="yellow"></v-tabs-slider>
+
+    <v-tab href="#tab-1">
+      IT
+      <v-icon>keyboard</v-icon>
+    </v-tab>
+
+    <v-tab href="#tab-2">
+      BCIO
+      <v-icon>settings_voice</v-icon>
+    </v-tab>
+
+    <v-tab href="#tab-3">
+      Office supplies
+      <v-icon>attach_file</v-icon>
+    </v-tab>
+
+    <v-tab-item
+      v-for="i in 3"
+      :value="'tab-' + i"
+      :key="i"
+    >
+       <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+      :pagination.sync="pagination"
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.name }}</td>
+        <td class="text-xs-right">{{ props.item.calories }}</td>
+        <td class="text-xs-right">{{ props.item.fat }}</td>
+        <td class="text-xs-right">{{ props.item.carbs }}</td>
+        <td class="text-xs-right">{{ props.item.protein }}</td>
+        <td class="text-xs-right">{{ props.item.iron }}</td>
+      </template>
+    </v-data-table>
+    <div class="text-xs-center pt-2">
+      <v-pagination v-model="pagination.page" :length="pages"></v-pagination>
+    </div>
+    </v-tab-item>
+  </v-tabs>
+    
+     
    </v-container>
    </div>
  
 </template>
 
 <script>
-  
-
+ 
   export default {
-    
-  }
+    data () {
+      return {
+        search: '',
+        pagination: {},
+        selected: [],
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'left',
+            sortable: false,
+            value: 'name'
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' }
+        ],
+        desserts: [
+          {
+            value: false,
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%'
+          },
+          {
+            value: false,
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%'
+          },
+          {
+            value: false,
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%'
+          },
+          {
+            value: false,
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%'
+          },
+          {
+            value: false,
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%'
+          },
+          {
+            value: false,
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%'
+          },
+          {
+            value: false,
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%'
+          },
+          {
+            value: false,
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%'
+          },
+          {
+            value: false,
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%'
+          },
+          {
+            value: false,
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%'
+          }
+        ],
+      tabs: null,
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+
+      }
+    },
+    computed: {
+      pages () {
+        if (this.pagination.rowsPerPage == null ||
+          this.pagination.totalItems == null
+        ) return 0
+
+        return Math.ceil(this.pagination.totalItems / this.pagination.rowsPerPage)
+      }
+    },
+
+       }
+
 </script>
